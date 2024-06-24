bcc22334e7af83aff1090020642abffdfa677f96
victorkane <victorkane@gmail.com>
2024-06-24 10:32:13 -0300

    style(theme): initial dark and light mode accent and text color definitions

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,4 +1,8 @@
-import colors from 'tailwindcss/colors';
+// Generated color palettes
+const accent = { 200: '#d9c5af', 600: '#945e0d', 900: '#442d0f', 950: '#30200f' };
+const gray = { 100: '#fff5e7', 200: '#ffeacf', 300: '#e9b975', 400: '#ba7d00', 500: '#774f00', 700: '#4e3200', 800: '#372200', 900: '#241500' };
+// import colors from 'tailwindcss/colors';
+
 import starlightPlugin from '@astrojs/starlight-tailwind';
 
 /** @type {import('tailwindcss').Config} */
@@ -6,12 +10,7 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
-			colors: {
-				// Your preferred accent color. Indigo is closest to Starlight’s defaults.
-				accent: colors.indigo,
-				// Your preferred gray scale. Zinc is closest to Starlight’s defaults.
-				gray: colors.zinc,
-			},
+			colors: { accent, gray },
 		},
 	},
 	plugins: [starlightPlugin()],
